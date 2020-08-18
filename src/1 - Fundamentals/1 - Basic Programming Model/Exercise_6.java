f3bacaeb17ba77f49aa3527ad13adbd9d2f43b3c
Maxim Vasilic <lmantenl@gmail.com>
2020-08-18 20:42:19 +0300

    1.1.6

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_6.java
@@ -0,0 +1,19 @@
+// Do not forget to install the textbook libraries!
+// How to do that you can find by following this
+// link: https://algs4.cs.princeton.edu/code/
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_6 {
+    public static void main(String[] args) {
+        // Guys do not be confused. It is an super nice algorithm
+        // that calculates first 16 Fibonacci numbers.
+
+        int f = 0;
+        int g = 1;
+        for (int i = 0; i <= 15; i++) {
+            StdOut.println(f);
+            f = f + g;
+            g = f - g;
+        }
+    }
+}
