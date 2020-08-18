e71f60c09ef83a71bc4f7316b2387ce85f3b0da3
Maxim Vasilic <lmantenl@gmail.com>
2020-08-18 20:20:34 +0300

    1.1.4

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_4.java
@@ -0,0 +1,21 @@
+public class Exercise_4 {
+    public static void main(String[] args) {
+        /*
+        a. if (a > b) then c = 0;
+        There is no such statement as 'then' in Java. Corrected
+        version: if (a > b) c = 0;
+
+        b. if a > b { c = 0; }
+        Condition (a > b) must be surrounded by parentheses. Right
+        way to do it: if (a > b) { c = 0; }
+
+        c. if (a > b) c = 0;
+        And we have the winner! That actually is one of the right
+        ways to write that IF statement.
+
+        d. if (a > b) c = 0 else b = 0;
+        The semicolon is not present after IF statement. Should be:
+        if (a > b) c = 0; else b = 0;
+        */
+    }
+}
