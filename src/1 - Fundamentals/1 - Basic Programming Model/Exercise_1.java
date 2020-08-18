476647b45aaa8d2c9d45f1798e5aa2f3f42ff461
Maxim Vasilic <lmantenl@gmail.com>
2020-08-18 19:31:05 +0300

    1.1.1

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_1.java
@@ -0,0 +1,30 @@
+@SuppressWarnings("ALL")
+public class Exercise_1 {
+    public static void main(String[] args) {
+        // The priority of each operation in this expression is the same as
+        // in regular math. The only tricky part is division. When you
+        // divide two numbers of type int, the result also will be int. For
+        // more info please check Integer division on wiki.
+        //
+        // op 1: 0 + 15 = 15
+        // op 2: 15 / 2 = 7
+        int a = (0 + 15) / 2;
+        System.out.println(a);
+
+        // How to solve this one:
+        //
+        // op 1: e-6 = 10 ^ -6 = 1 / 1000000
+        // op 2: 1 / 1000000.0 * 100000000.1 = 100.1
+        // op 3: 100.1 * 2.0 = 200.2
+        double b = 2.0e-6 * 100000000.1;
+        System.out.println(b);
+
+        // Boolean comparison... OK. Lets do it!
+        //
+        // op 1: true && false = false
+        // op 2: true && true = true
+        // op 3: false || true = true
+        boolean c = true && false || true && true;
+        System.out.println(c);
+    }
+}
