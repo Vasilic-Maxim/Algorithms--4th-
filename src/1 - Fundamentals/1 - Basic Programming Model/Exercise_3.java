3a873884236f5662f15cfc819a92cd69e40ba86b
Maxim Vasilic <lmantenl@gmail.com>
2020-08-18 20:07:19 +0300

    1.1.3

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_3.java
@@ -0,0 +1,15 @@
+public class Exercise_3 {
+    public static void main(String[] args) {
+        // If you are working from IntelliJ IDEA:
+        // 1 - right click on file or code area
+        // 2 - select "Open in Terminal" option
+        // 3 - write "java Exercise_3.java 1 1 2"
+        // 4 - Enjoy...
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if (a == b && b == c) System.out.println("Equal!");
+        else System.out.println("Not equal!");
+    }
+}
