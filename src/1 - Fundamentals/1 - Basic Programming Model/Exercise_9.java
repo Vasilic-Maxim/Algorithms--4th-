7c73cc6e41a8edd64c67079a331293a6f9b1c5fd
Maxim Vasilic <lmantenl@gmail.com>
2020-08-19 11:23:53 +0300

    1.1.9

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_9.java
@@ -0,0 +1,26 @@
+public class Exercise_9 {
+    public static String toBinaryString1(int n) {
+        // Probably the most obvious way to do that is to just
+        // call built-in method of Integer class.
+        return Integer.toBinaryString(n);
+    }
+
+    public static String toBinaryString2(int n) {
+        // To know that some API exists is great. But can you
+        // how it solves the problem? By the way if you are
+        // wandering why I use StringBuilder() instead of
+        // concatenation with "+" sign, please check out this
+        // link: https://stackoverflow.com/questions/1532461/
+        StringBuilder s = new StringBuilder();
+        for (int div = n; div > 0; div /= 2)
+            s.append(div % 2);
+
+        return s.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        int num = 100000;
+        System.out.println(toBinaryString1(num));
+        System.out.println(toBinaryString2(num));
+    }
+}
