242705e2fd37d2ab7bf20cc9ba6e0aeef217b834
Maxim Vasilic <lmantenl@gmail.com>
2020-08-19 19:39:00 +0300

    1.1.14

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_14.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Exercise_14 {
+    public static int lg(int num) {
+        // To find logarithm base 2 of a number, divide the
+        // number by 2 until it becomes 1. Why one? I know it
+        // is not polite to answer with a question, but what
+        // power of 2 gives us 1? If your answer is 0 then
+        // you got it right!
+        int base = 0;
+        for (; num > 1; num /= 2) base++;
+        return base;
+    }
+
+    public static void main(String[] args) {
+        int initial = StdRandom.uniform(100);
+        StdOut.printf("The logarithm base 2 of %d = %d", initial, lg(initial));
+    }
+}
