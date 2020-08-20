664753d86b12a53d52eedd711c59874cb6b7e5e2
Maxim Vasilic <lmantenl@gmail.com>
2020-08-20 21:56:36 +0300

    1.1.22

---
+++ src/1 - Fundamentals/1 - Basic Programming Model/Exercise_22.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_22 {
+    public static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length - 1, 1);
+    }
+
+    public static int rank(int key, int[] a, int lo, int hi, int depth) {
+        StdOut.printf("%s [lo, hi] = [%d, %d]%n", "*".repeat(depth), lo, hi);
+        if (lo > hi) return -1;
+
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) return rank(key, a, lo, mid - 1, depth + 1);
+        else if (key > a[mid]) return rank(key, a, mid + 1, hi, depth + 1);
+        else return mid;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 4;
+        StdOut.printf("Result: %d", rank(target, array));
+    }
+}
