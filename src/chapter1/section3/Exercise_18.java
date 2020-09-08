361b2f0af7e21aab70c3222d0d1bd6f04200ec5e
Maxim Vasilic <lmantenl@gmail.com>
2020-09-08 16:55:14 +0300

    1.3.18

---
+++ src/chapter1/section3/Exercise_18.java
@@ -0,0 +1,8 @@
+package chapter1.section3;
+
+public class Exercise_18 {
+    /*
+    * This code changes deletes next element from the Linked List by
+    * changing reference in current node.
+    * */
+}
